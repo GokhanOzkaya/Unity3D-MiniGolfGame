6cc998be2662ab037010b849987359e0f10c0224
Gökhan Özkaya <95971326+GokhanOzkaya@users.noreply.github.com>
2022-12-10 23:11:43 +0300

    MiniGolf_V1.0.0

---
+++ My project/Assets/Scripts/BallController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BallController : MonoBehaviour
+{
+    public float maxPower;
+    public float cahngeAngleSpeed;
+    public float lineLength;
+    public Slider powerSlider;
+    public TextMeshProUGUI puttCountLabel;
+
+    private LineRenderer line;
+    private Rigidbody ball;
+    private float angle;
+    private float powerUpTime;
+    private float power;
+    private int putts;
+
+
+    void Awake()
+    {
+        ball = GetComponent<Rigidbody>();
+        ball.maxAngularVelocity = 1000;
+        line = GetComponent<LineRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ChangeAngle(-1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            ChangeAngle(1);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Putt();
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PowerUp();
+        }
+        
+     
+        UpdateLinePositions();
+    }
+
+    private void ChangeAngle(int direction)
+    {
+        angle += cahngeAngleSpeed * Time.deltaTime * direction;
+    }
+
+    private void UpdateLinePositions()
+    {
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, transform.position + Quaternion.Euler(0, angle, 0) * Vector3.forward * lineLength);
+    }
+
+    private void Putt()
+    {
+        ball.AddForce(Quaternion.Euler(0, angle, 0) * Vector3.forward  *  maxPower * power, ForceMode.Impulse);
+        power = 0;
+        powerSlider.value = 0;
+        powerUpTime = 0;
+        putts++;
+        puttCountLabel.text = putts.ToString();
+            
+    }
+   
+    private void PowerUp()
+    {
+        powerUpTime += Time.deltaTime;
+        power = Mathf.PingPong(powerUpTime,1);
+        powerSlider.value = power;
+    }
+}
